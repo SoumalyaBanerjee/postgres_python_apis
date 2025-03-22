50e31010023e8dd0806ee59333b179eaf078a4f9
SoumalyaBanerjee <40776477+SoumalyaBanerjee@users.noreply.github.com>
2025-03-22 15:03:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def get_db_connection():
 
 # API: Bulk Insert Users
 @app.post("/users/bulk-insert")
-def bulk_insert_users(users: List[User]):
+def bulk_insert_users(users: list[dict]):
     conn = get_db_connection()
     cursor = conn.cursor()
     try:
